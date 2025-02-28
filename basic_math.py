590bf9f5f2d7ac7f6c056084ed44b749e308aaf4
Chanella Keza <chanella.keza@stu.ocsb.ca>
2025-02-28 16:36:16 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Chanella
+# Date: Feb 28, 2025
+# This program does math
+
+
+# this function calculates the area and perimeter
+def main():
+    print("4*2={}".format(4 * 2))
+    print("9+2={}".format(9 + 2))
+    print("9-4={}".format(9 - 4))
+    print("6/3={}".format(6 / 3))
+    print("6+6/2={}".format(6 + 6 / 2))
+    print("4+2*3={}".format(4 + 2 * 3))
+    print("7+3^3={}".format(7 + 3 ^ 3))
+    print("8+3**2={}".format(8 + 3**2))
+
+
+if __name__ == "__main__":
+    main()
